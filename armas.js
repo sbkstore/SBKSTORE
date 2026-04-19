534ff3f18a4fad21de3e8c4dd26fde798a11c7d4
sbkstore <sbkstore458@gmail.com>
2026-04-19 01:30:22 -0300

    Add files via upload

---
+++ armas.js
@@ -0,0 +1,12 @@
+productos.armas = [
+  { titulo: "glock", precio: "$50.000", etiqueta: "REPLICA", descripcion: "glock replica polimero.", imagenes: ["armas/fotos/1.jpg"] },
+  { titulo: "Glock", precio: "$950.000", etiqueta: "EN CAJA", descripcion: "Glock 17 de Generación 3 (Gen 3)Semiautomática + 90 balas.", imagenes: ["armas/fotos/2.jpg"] },
+  { titulo: "Mahely", precio: "$360.000", etiqueta: "CAL 22", descripcion: "Mahely M21 CAL 22 + cargador lleno.", imagenes: ["armas/fotos/3.jpg"] },
+  { titulo: "SMY", precio: "$195.000", etiqueta: "CAL 38", descripcion: "Smith & Wesson CAL 38.", imagenes: ["armas/fotos/4.jpg"] },
+  { titulo: "colt", precio: "$420.000", etiqueta: "NUEVO", descripcion: "colt 1927 + cargador lleno.", imagenes: ["armas/fotos/5.jpg"] },
+  { titulo: "beretta", precio: "$570.000", etiqueta: "CAL 9", descripcion: "Beretta 92FS + cargador lleno.", imagenes: ["armas/fotos/6.jpg"] },
+  { titulo: "bersa", precio: "$500.000", etiqueta: "NUEVO", descripcion: "Bersa Thunder CAL. .40 S&W.", imagenes: ["armas/fotos/7.jpg"] },
+  { titulo: "revolver", precio: "$705.000", etiqueta: "3 JUNTAS", descripcion: "revolvers cargadores llenos x3", imagenes: ["armas/fotos/8.jpg"] },
+  { titulo: "ruby", precio: "$200.000", etiqueta: "NUEVO", descripcion: "Ruby 2 cargadores 1 lleno 32 ACP.", imagenes: ["armas/fotos/9.jpg"] },
+  { titulo: "fierros", precio: "$1.500.000", etiqueta: "HOT 2 JUNTAS", descripcion: "Beretta 92FS y una 2011 cargadores llenos.", imagenes: ["armas/fotos/10.jpg"] }
+];
---
+++ productos.js
@@ -0,0 +1,14 @@
+const productos = {
+  catalogo: [
+    { titulo: "RUCU CUCU OG", precio: "$45.000", etiqueta: "FOTO/FEM 5 unidades", descripcion: "Seedstockers,SAT 50% IND 50% THC 30%.", imagenes: ["plantas/fotos/1.jpg"] },
+    { titulo: "ORANGE BUD", precio: "$45.000", etiqueta: "FOTO/FEM 5 unidades", descripcion: "Dutch Passion,SAT 70% IND 30% THC 15%.", imagenes: ["plantas/fotos/2.jpg"] },
+    { titulo: "GLUE BERRY", precio: "$50.000", etiqueta: "FOTO/FEM 5 unidades", descripcion: "Dutch Passion,SAT 50% IND 50% THC 15%.", imagenes: ["plantas/fotos/3.jpg"] },
+    { titulo: "OH MY GUSHER", precio: "$36.000", etiqueta: "AUTO 5 unidades", descripcion: "Dutch Passion,SAT 30% IND 70% THC 20%.", imagenes: ["plantas/fotos/4.jpg"] },
+    { titulo: "CANDY BUBATZ XL", precio: "$150.000", etiqueta: "AUTO x 50 unidades", descripcion: "Dutch Passion,SAT 30% IND 70% THC 20%.", imagenes: ["plantas/fotos/5.jpg"] },
+    { titulo: "GMO FAST", precio: "$45.000", etiqueta: "FOTO/FEM 5 unidades", descripcion: "Dutch Passion,SAT 50% IND 50% THC 23%.", imagenes: ["plantas/fotos/6.jpg"] },
+    { titulo: "SELENE REG", precio: "$55.000", etiqueta: "REGULAR 20 unidades", descripcion: "Criadores del Rio De La Plata,SAT 50% IND 50% THC 23%.", imagenes: ["plantas/fotos/7.jpg"] },
+    { titulo: "GOLDEN LEMON HAZE", precio: "$35.000", etiqueta: "FOTO/FEM 5 unidades", descripcion: "Seedstockers,SAT 60% IND 40% THC 23%.", imagenes: ["plantas/fotos/8.jpg"] },
+    { titulo: "AUTO GELATO", precio: "$44.000", etiqueta: "AUTO 5 unidades", descripcion: "Seedstockers,SAT 20% IND 80% THC 20%.", imagenes: ["plantas/fotos/9.jpg"] },
+    { titulo: "JACK HERER", precio: "$37.000", etiqueta: "FOTO/FEM 5 unidades", descripcion: "Seedstockers,SAT 70% IND 30% THC 23%.", imagenes: ["plantas/fotos/10.jpg"] }
+  ]
+};

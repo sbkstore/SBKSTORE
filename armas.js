1b4a091b83e0339755fa96195cf7886edd74a66e
sbkstore <sbkstore458@gmail.com>
2026-04-22 20:31:07 -0300

    Add files via upload

--- armas.js
+++ armas.js
@@ -8,5 +8,7 @@ productos.armas = [
   { titulo: "bersa", precio: "$500.000", etiqueta: "NUEVO", descripcion: "Bersa Thunder CAL. .40 S&W.", imagenes: ["armas/fotos/7.jpg"] },
   { titulo: "revolver", precio: "$705.000", etiqueta: "3 JUNTAS", descripcion: "revolvers cargadores llenos x3", imagenes: ["armas/fotos/8.jpg"] },
   { titulo: "ruby", precio: "$200.000", etiqueta: "NUEVO", descripcion: "Ruby 2 cargadores 1 lleno 32 ACP.", imagenes: ["armas/fotos/9.jpg"] },
-  { titulo: "fierros", precio: "$1.500.000", etiqueta: "HOT 2 JUNTAS", descripcion: "Beretta 92FS y una 2011 cargadores llenos.", imagenes: ["armas/fotos/10.jpg"] }
+  { titulo: "fierros", precio: "$1.500.000", etiqueta: "HOT 2 JUNTAS", descripcion: "Beretta 92FS y una 2011 cargadores llenos.", imagenes: ["armas/fotos/10.jpg"] },
+  { titulo: "revolver", precio: "$100.000", etiqueta: "38", descripcion: "38 tambor lleno + balas.", imagenes: ["armas/fotos/11.jpg"] },
+  { titulo: "Colt", precio: "$50.000", etiqueta: "REPLICA", descripcion: "COLT REPLICA A AIRE BALINES METAL.", imagenes: ["armas/fotos/12.jpg"] }
 ];
